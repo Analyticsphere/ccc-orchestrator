f97764ea35ea50b815c2fd180936e4a53fe82a95
Jake Peters <jcbptrs@gmail.com>
2025-01-15 13:46:31 -0500

    fixed file_validation API params

--- dags/dependencies/validation.py
+++ dags/dependencies/validation.py
@@ -5,7 +5,7 @@ import requests
 def validate_file(file_path: str, omop_version: str) -> None:
     utils.logger.info(f"Validating schema of {file_path} against OMOP v{omop_version}")
     response = requests.get(
-        f"{constants.PROCESSOR_ENDPOINT}/validate_file?file_path={file_path}?omop_version={omop_version}",
+        f"{constants.PROCESSOR_ENDPOINT}/validate_file?file_path={file_path}&omop_version={omop_version}",
         headers=utils.get_auth_header()
     )
     response.raise_for_status()
