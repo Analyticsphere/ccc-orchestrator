2b5ed9ae734f2caf5c28a70141ebe48078c6957b
Jake Peters <jcbptrs@gmail.com>
2025-01-15 11:52:40 -0500

    fix file path argument

--- dags/dependencies/validation.py
+++ dags/dependencies/validation.py
@@ -2,10 +2,10 @@ from . import utils
 from . import constants
 import requests
 
-def validate_file(gcs_file_path: str, omop_version: str) -> None:
-    utils.logger.info(f"Validating schema of {gcs_file_path} against OMOP v{omop_version}")
+def validate_file(file_path: str, omop_version: str) -> None:
+    utils.logger.info(f"Validating schema of {file_path} against OMOP v{omop_version}")
     response = requests.get(
-        f"{constants.PROCESSOR_ENDPOINT}/validate_file?gcs_file_path={gcs_file_path}?omop_version={omop_version}",
+        f"{constants.PROCESSOR_ENDPOINT}/validate_file?file_path={file_path}?omop_version={omop_version}",
         headers=utils.get_auth_header()
     )
     response.raise_for_status()
