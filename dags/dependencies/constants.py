f08a85ac21eb314b29c0db16bb90ed4bc961336d
Eddy Frankenberger <edward.frankenberger@nih.gov>
2025-01-29 14:52:32 -0500

    switch processor endpoint

--- dags/dependencies/constants.py
+++ dags/dependencies/constants.py
@@ -1,7 +1,6 @@
 from enum import Enum
 
-#PROCESSOR_ENDPOINT = "https://ccc-omop-file-processor-1061430463455.us-central1.run.app"
-PROCESSOR_ENDPOINT = "https://ccc-omop-file-processor-jp-1061430463455.us-central1.run.app"
+PROCESSOR_ENDPOINT = "https://ccc-omop-file-processor-1061430463455.us-central1.run.app"
 SITE_CONFIG_YML_PATH = "/home/airflow/gcs/dags/config/site_config.yml"
 
 class FileConfig(str, Enum):
