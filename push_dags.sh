b9e3d19f7d67f7add9f91ec7a0689b813ad7c617
Jake Peters <jcbptrs@gmail.com>
2025-02-07 14:14:09 -0500

    push_dags.sh

---
+++ push_dags.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# push_dags.sh: Pushes Airflow DAG files to your *dev* Composer environment.
+
+set -e
+
+# Configure for dev environment
+PROJECT_ID="nih-nci-dceg-connect-dev"
+COMPOSER_BUCKET="us-central1-ccc-jp-dev-54b9b374-bucket"
+
+echo "Setting GCP project to: $PROJECT_ID"
+gcloud config set project "$PROJECT_ID"
+
+echo "Copying local DAGs to dev Composer bucket..."
+gsutil -m cp -r dags/* gs://"$COMPOSER_BUCKET"/dags/
+
+echo "Done. DAGs have been pushed to dev environment!"
+ 
