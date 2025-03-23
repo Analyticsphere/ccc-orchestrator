e9f2aa9ddbf6aff9b854ca38b1f5cdd0db87bcce
Jake Peters <jcbptrs@gmail.com>
2025-03-22 20:18:34 -0400

    update params for gcs_artifacts_path

---
+++ dags/dependencies/ehr/analysis.py
@@ -0,0 +1,27 @@
+'''Run HADES R packages to analyze the OMOP data, e.g., DataQualityDashboard and Achilles.'''
+from dependencies.ehr import utils
+from dependencies.ehr import constants
+
+def run_dqd(project_id: str, dataset_id: str, gcs_artifact_path: str) -> None:
+    utils.logger.info(f"Running dqd_checks for {project_id}.{dataset_id}")
+    utils.make_api_call(
+        url = constants.ANALAYZER_URL,
+        endpoint="run_dqd",
+        json_data={
+            "project_id": project_id,
+            "dataset_id": dataset_id,
+            "gcs_artifact_path": gcs_artifact_path               
+        }
+    )
+
+def run_achilles(project_id: str, dataset_id: str, gcs_artifact_path: str) -> None:
+    utils.logger.info(f"Running Achilles for {project_id}.{dataset_id}")
+    utils.make_api_call(
+        url = constants.ANALAYZER_URL,
+        endpoint="run_achilles",
+        json_data={
+            "project_id": project_id,
+            "dataset_id": dataset_id,  
+            "gcs_artifact_path": gcs_artifact_path          
+        }
+    )
