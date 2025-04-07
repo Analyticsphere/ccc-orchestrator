fd590854a059a3fe286fe98566d9ac8635a25d1e
Jake Peters <jcbptrs@gmail.com>
2025-04-07 11:37:34 -0400

    add url param to harmonize()

--- dags/dependencies/ehr/vocab.py
+++ dags/dependencies/ehr/vocab.py
@@ -1,9 +1,11 @@
 from dependencies.ehr import utils
+from dependencies.ehr import constants
 
 
 def load_vocabulary_table_gcs_to_bq(vocab_version: str, vocab_gcs_bucket: str, table_file_name: str, project_id: str, dataset_id: str) -> None:
     utils.logger.info(f"Loading {table_file_name} vocabulary table to {project_id}.{dataset_id}")
     utils.make_api_call(
+        url=constants.PROCESSOR_URL,
         endpoint="load_target_vocab",
         json_data={
             "vocab_version": vocab_version,
@@ -19,6 +21,7 @@ def create_optimized_vocab(vocab_version: str, vocab_gcs_bucket: str) -> None:
     utils.logger.info(f"Creating optimized version of {vocab_version} if required")
 
     utils.make_api_call(
+        url=constants.PROCESSOR_URL,
         endpoint="create_optimized_vocab",
         json_data={
             "vocab_version": vocab_version,
@@ -30,6 +33,7 @@ def harmonize(vocab_version: str, vocab_gcs_bucket: str, omop_version: str, file
     utils.logger.info(f"Standardizing {file_path} to vocabulary version {vocab_version}")
 
     utils.make_api_call(
+        url=constants.PROCESSOR_URL,
         endpoint="harmonize_vocab",
         json_data={
             "vocab_version": vocab_version,
