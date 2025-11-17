f9bfe798d154ce00421430f13d47e89e6cc9dcfe
Eddy Frankenberger <edward.frankenberger@nih.gov>
2025-11-17 18:05:04 -0500

    add achilles

--- dags/ehr_pipeline.py
+++ dags/ehr_pipeline.py
@@ -658,6 +658,38 @@ def dqd(site_to_process: tuple[str, str]) -> None:
         bq.bq_log_error(site, delivery_date, utils.get_run_id(get_current_context()), str(e))
         raise Exception(error_msg) from e
 
+@task(max_active_tis_per_dag=10, trigger_rule="none_failed", execution_timeout=timedelta(hours=3))
+def achilles(site_to_process: tuple[str, str]) -> None:
+
+    site, delivery_date = site_to_process
+    bq.bq_log_running(site, delivery_date, utils.get_run_id(get_current_context()))
+
+    try:
+        utils.logger.info(f"Triggering Achilles analyses for {site} data delivered on {delivery_date}")
+
+        project_id = utils.get_site_config_file()[constants.FileConfig.SITE.value][site][constants.FileConfig.PROJECT_ID.value]
+        dataset_id = utils.get_site_config_file()[constants.FileConfig.SITE.value][site][constants.FileConfig.BQ_DATASET.value]
+        gcs_bucket = utils.get_site_config_file()[constants.FileConfig.SITE.value][site][constants.FileConfig.GCS_BUCKET.value]
+        cdm_source_name = utils.get_site_config_file()[constants.FileConfig.SITE.value][site][constants.FileConfig.DISPLAY_NAME.value]
+        artifact_path = constants.ArtifactPaths.ACHILLES.value
+        gcs_artifact_path = os.path.join(gcs_bucket, delivery_date, artifact_path)
+        cdm_version = constants.OMOP_TARGET_CDM_VERSION
+
+        # Execute Achilles via Cloud Run Job
+        analysis.run_achilles_job(
+            project_id=project_id,
+            dataset_id=dataset_id,
+            gcs_artifact_path=gcs_artifact_path,
+            cdm_version=cdm_version,
+            cdm_source_name=cdm_source_name,
+            context=get_current_context()
+        )
+
+    except Exception as e:
+        error_msg = f"Unable to run Achilles: {e}"
+        bq.bq_log_error(site, delivery_date, utils.get_run_id(get_current_context()), str(e))
+        raise Exception(error_msg) from e
+
 @task(trigger_rule=TriggerRule.ALL_DONE, retries=0)
 def log_done() -> None:
     # This final task will run regardless of previous task states.
@@ -736,6 +768,9 @@ with dag:
     # Run Data Quality Dashboard after loading data
     run_dqd = dqd.expand(site_to_process=unprocessed_sites)
 
+    # Run Achilles analyses after loading data
+    run_achilles = achilles.expand(site_to_process=unprocessed_sites)
+
     # Final log_done task runs regardless of task outcomes.
     all_done = log_done()
     
@@ -750,7 +785,7 @@ with dag:
     vocab_harmonization_group >> load_to_bigquery_group
     
     # Continue with remaining tasks loading dataset
-    load_to_bigquery_group >> derived_data >> cleanup 
-    
-    # Run analytics tasks
-    cleanup >> run_dqd >> all_done
+    load_to_bigquery_group >> derived_data >> cleanup
+
+    # Run analytics tasks in parallel
+    cleanup >> [run_dqd, run_achilles] >> all_done
